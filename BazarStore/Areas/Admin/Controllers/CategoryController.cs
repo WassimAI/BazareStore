8870daf89e9758a6e49edbd34af4f5392036ac8e
wassimAI - Wassim Ibrahim <40058827+WassimInstaclass@users.noreply.github.com>
2019-11-10 17:52:08 +0400

    Started with Admin Area - Categories (Get all and create)

---
+++ BazarStore/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using BazarStore.Areas.Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BazarStore.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private BazarEntities db = new BazarEntities();
+        // GET: Admin/Category
+        public ActionResult GetCategories()
+        {
+            var categoryList = new List<CategoryVM>();
+            categoryList = db.Categories.ToArray().Select(x => new CategoryVM(x)).ToList();
+
+            return View(categoryList);
+        }
+
+        public ActionResult Create()
+        {
+            var model = new CategoryVM();
+
+            return View(model);
+        }
+    }
+}
---
+++ BazarStore/Areas/Admin/Models/CategoryVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BazarStore.Areas.Admin.Models
+{
+    public class CategoryVM
+    {
+        public CategoryVM()
+        {
+
+        }
+
+        public CategoryVM(Category row)
+        {
+            CategoryID = row.CategoryID;
+            CategoryName = row.CategoryName;
+            Description = row.Description;
+            ImageUrl = row.ImageUrl;
+        }
+
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
